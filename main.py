6a6aa82c1f2e25cc3329aefaa55eb9d174c93cd0
CozmixOff <cozmix.n2@gmail.com>
2022-04-15 23:10:53 +0200

    Added music for menu

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame
+from pygame import mixer
 
+mixer.init()
 pygame.init()
 
 pygame.display.set_caption("No One Needs Five")
@@ -7,13 +9,16 @@ screen = pygame.display.set_mode((1080, 720))
 
 background = pygame.image.load('assets/graphics/Multi_Platformer_Tileset_v2/GrassLand/Background/GrassLand_Background_Guide.png')
 background = pygame.transform.scale(background, (1080, 720))
-pygame.display.set_caption('Button Demo')
-start_img = pygame.image.load('assets/graphics/custom')
+mixer.music.load("assets/musics/Pleasant_Adventure_Pre.wav")
+mixer.music.play()
 
 running = True
 while running:
     screen.blit(background, (0, 0))
     pygame.display.flip()
+    if not mixer.music.get_busy():
+        mixer.music.load("assets/musics/Pleasant_Adventure_Loop.wav")
+        mixer.music.play()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
