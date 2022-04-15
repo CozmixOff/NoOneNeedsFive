94d08cf27e1ad278bcc8e5666146e03a2c0b0858
CozmixOff <cozmix.n2@gmail.com>
2022-04-15 22:33:07 +0200

    Updated

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 import pygame
+
 pygame.init()
 
 pygame.display.set_caption("No One Needs Five")
 screen = pygame.display.set_mode((1080, 720))
-background = pygame.image.load('assets/Multi_Platformer_Tileset_v2/GrassLand/Background/GrassLand_Background_Guide.png')
-background = pygame.transform.scale(background, (1080,720))
+
+background = pygame.image.load('assets/graphics/Multi_Platformer_Tileset_v2/GrassLand/Background/GrassLand_Background_Guide.png')
+background = pygame.transform.scale(background, (1080, 720))
+pygame.display.set_caption('Button Demo')
+start_img = pygame.image.load('assets/graphics/custom')
 
 running = True
 while running:
-    screen.blit(background, (0,0))
+    screen.blit(background, (0, 0))
     pygame.display.flip()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
