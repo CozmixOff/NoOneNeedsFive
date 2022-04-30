6b175aa348007d43f915b90ebd89d9d913ca67c7
CozmixOff <cozmix.n2@gmail.com>
2022-04-30 23:19:16 +0200

    Animated background clouds

--- game/main.py
+++ game/main.py
@@ -8,11 +8,35 @@ pygame.init()
 window_x = 1080
 window_y = 720
 
+pygame_icon = pygame.image.load("../assets/graphics/MinifolksHumans/modified/icon.png")
+pygame.display.set_caption("No One Needs Five")
+screen = pygame.display.set_mode((window_x, window_y))
+pygame.display.set_icon(pygame_icon)
+
+cloud1_coords = 0.30 * window_x
+cloud2_coords = 0.07 * window_x
+cloud3_coords = 0.70 * window_x
+
 intro_screen.IntroScreen.init()
 
 running = True
 while running:
-    intro_screen.IntroScreen.run(window_x, window_y)
+    intro_screen.IntroScreen.run(window_x, window_y,cloud1_coords, cloud2_coords, cloud3_coords, screen)
+
+    # clouds animation
+
+    cloud1_coords -= 0.5
+    cloud2_coords -= 0.5
+    cloud3_coords -= 0.5
+    if cloud1_coords <= -0.13*window_x:
+        cloud1_coords = window_x
+    elif cloud2_coords <= -0.17*window_x:
+        cloud2_coords = window_x
+    elif cloud3_coords <= -0.26*window_x:
+        cloud3_coords = window_x
+
+    # checking closed window
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
--- game/screens/intro_screen.py
+++ game/screens/intro_screen.py
@@ -7,23 +7,35 @@ class IntroScreen:
     def init():
         mixer.music.load("../assets/musics/Pleasant_Adventure_Pre.wav")
         mixer.music.play()
-    @staticmethod
-    def run(window_x, window_y):
-        pygame_icon = pygame.image.load("../assets/graphics/MinifolksHumans/icon.png")
-        pygame.display.set_caption("No One Needs Five")
-        screen = pygame.display.set_mode((window_x, window_y))
-        pygame.display.set_icon(pygame_icon)
 
-        background = pygame.image.load(
-            '../assets/graphics/Multi_Platformer_Tileset_v2/GrassLand/Background/GrassLand_Background_Guide.png')
-        background = pygame.transform.scale(background, (window_x, window_y))
+    @staticmethod
+    def run(window_x, window_y, cloud1_coords, cloud2_coords, cloud3_coords, screen):
+        intro_background = pygame.image.load(
+            '../assets/graphics/Multi_Platformer_Tileset_v2/modified/intro_animation/intro_background.png')
+        intro_background = pygame.transform.scale(intro_background, (window_x, window_y))
+
+        screen.blit(intro_background, (0, 0))
+
+        cloud1 = pygame.image.load(
+            '../assets/graphics/Multi_Platformer_Tileset_v2/GrassLand/Background/GrassLand_Cloud_1.png')
+        cloud1 = pygame.transform.scale(cloud1, (0.13 * window_x, 0.07 * window_y))
+        cloud2 = pygame.image.load(
+            '../assets/graphics/Multi_Platformer_Tileset_v2/GrassLand/Background/GrassLand_Cloud_2.png')
+        cloud2 = pygame.transform.scale(cloud2, (0.17 * window_x, 0.15 * window_y))
+        cloud3 = pygame.image.load(
+            '../assets/graphics/Multi_Platformer_Tileset_v2/GrassLand/Background/GrassLand_Cloud_3.png')
+        cloud3 = pygame.transform.scale(cloud3, (0.26 * window_x, 0.15 * window_y))
+
+        screen.blit(cloud1, (cloud1_coords, 0.04*window_y))
+        screen.blit(cloud2, (cloud2_coords, 0.19*window_y))
+        screen.blit(cloud3, (cloud3_coords, 0.30*window_y))
 
         game_logo = pygame.image.load('../assets/graphics/custom/game_logo.png')
         game_logo = pygame.transform.scale(game_logo, (0.45 * window_x, 0.23 * window_y))
-
-        screen.blit(background, (0, 0))
         screen.blit(game_logo, ((window_x - 0.45 * window_x) / 2, 0.15 * window_y))
-        pygame.display.flip()
+
         if not mixer.music.get_busy():
             mixer.music.load("../assets/musics/Pleasant_Adventure_Loop.wav")
             mixer.music.play()
+
+        pygame.display.flip()
